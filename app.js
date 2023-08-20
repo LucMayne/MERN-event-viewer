f031b9791dbecccc18925d71ae331ff2a2355e52
LucMayne <54806836+LucMayne@users.noreply.github.com>
2023-08-20 18:42:32 +0200

    Update app.js, updated the build after deploying to heroku

--- app.js
+++ app.js
@@ -41,10 +41,13 @@ app.use(
 );
 
 // build
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
     app.use(express.static(path.join(__dirname, 'frontend/build')));
 }
-app.use(express.static(path.join(__dirname,'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use(logger('dev'));
 app.use(express.json());
